bfd8c8582cf614cf8d7e7ff8a79702a4b9b9501b
Ritu Parwal <ritu.parwal@gmail.com>
2019-11-06 14:02:51 -0800

    Updates

---
+++ precache-manifest.adcca4d9a655c6a50b13c807053b3e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1480de57fb9cdf27267d07671aa3d89",
+    "url": "/Monsters_Rolodex/index.html"
+  },
+  {
+    "revision": "85896841f1d7fa20aa42",
+    "url": "/Monsters_Rolodex/static/css/main.7ac43d15.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/Monsters_Rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "85896841f1d7fa20aa42",
+    "url": "/Monsters_Rolodex/static/js/main.99539292.chunk.js"
+  },
+  {
+    "revision": "b74efb52d6c617b4231f",
+    "url": "/Monsters_Rolodex/static/js/runtime-main.d42736ce.js"
+  }
+]);
